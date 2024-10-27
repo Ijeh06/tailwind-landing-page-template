1ade34f87fcda3e5ac26330299c77ab2092e4c13
Ijeh Ekene <ijehekene404@gmail.com>
2024-10-27 07:11:11 +0100

    first commit

---
+++ components/ui/footer.tsx
@@ -0,0 +1,214 @@
+import Link from "next/link";
+import Logo from "./logo";
+
+export default function Footer({ border = false }: { border?: boolean }) {
+  return (
+    <footer>
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        {/* Top area: Blocks */}
+        <div
+          className={`grid gap-10 py-8 sm:grid-cols-12 md:py-12 ${border ? "border-t [border-image:linear-gradient(to_right,transparent,theme(colors.slate.200),transparent)1]" : ""}`}
+        >
+          {/* 1st block */}
+          <div className="space-y-2 sm:col-span-12 lg:col-span-4">
+            <div>
+              <Logo />
+            </div>
+            <div className="text-sm text-gray-600">
+              &copy; 2023 Dutiful®
+            </div>
+          </div>
+
+          {/* 2nd block */}
+          <div className="space-y-2 sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h3 className="text-sm font-medium">Quick-Service</h3>
+            <ul className="space-y-2 text-sm">
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Features
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Integrations
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Pricing & Plans
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Changelog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Our method
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* 3rd block */}
+          <div className="space-y-2 sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h3 className="text-sm font-medium">Company</h3>
+            <ul className="space-y-2 text-sm">
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  About us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Diversity & Inclusion
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Careers
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Financial statements
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* 4th block */}
+          <div className="space-y-2 sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h3 className="text-sm font-medium">Resources</h3>
+            <ul className="space-y-2 text-sm">
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Community
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Terms of service
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="text-gray-600 transition hover:text-purple-900"
+                  href="#0"
+                >
+                  Report a vulnerability
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* 5th block */}
+          <div className="space-y-2 sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h3 className="text-sm font-medium">Social</h3>
+            <ul className="flex gap-1">
+              <li>
+                <Link
+                  className="flex items-center justify-center text-purple-500 transition hover:text-purple-600"
+                  href="#0"
+                  aria-label="Twitter"
+                >
+                  <svg
+                    className="h-8 w-8 fill-current"
+                    viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z"></path>
+                  </svg>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="flex items-center justify-center text-purple-500 transition hover:text-purple-600"
+                  href="#0"
+                  aria-label="Medium"
+                >
+                  <svg
+                    className="h-8 w-8 fill-current"
+                    viewBox="0 0 32 32"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M23 8H9a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1Zm-1.708 3.791-.858.823a.251.251 0 0 0-.1.241V18.9a.251.251 0 0 0 .1.241l.838.823v.181h-4.215v-.181l.868-.843c.085-.085.085-.11.085-.241v-4.887l-2.41 6.131h-.329l-2.81-6.13V18.1a.567.567 0 0 0 .156.472l1.129 1.37v.181h-3.2v-.181l1.129-1.37a.547.547 0 0 0 .146-.472v-4.749a.416.416 0 0 0-.138-.351l-1-1.209v-.181H13.8l2.4 5.283 2.122-5.283h2.971l-.001.181Z"></path>
+                  </svg>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="flex items-center justify-center text-purple-500 transition hover:text-purple-600"
+                  href="#0"
+                  aria-label="Github"
+                >
+                  <svg
+                    className="h-8 w-8 fill-current"
+                    viewBox="0 0 48 48"
+                     xmlns="http://www.w3.org/2000/svg"
+                    > 
+                     <path fill="#8C00FBC4" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"></path><path fill="#FFF" d="M20 31L20 17 32 24z"></path>
+                  </svg>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Big text */}
+      <div className="relative -mt-16 h-60 w-full" aria-hidden="true">
+        <div className="pointer-events-none absolute left-1/2 -z-10 -translate-x-1/2 text-center text-[348px] font-bold leading-none before:bg-gradient-to-b before:from-gray-200 before:to-gray-100/30 before:to-80% before:bg-clip-text before:text-transparent before:content-['Simple'] after:absolute after:inset-0 after:bg-gray-300/70 after:bg-clip-text after:text-transparent after:mix-blend-darken after:content-['Simple'] after:[text-shadow:0_1px_0_white]"></div>
+        {/* Glow */}
+        <div
+          className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-2/3"
+          aria-hidden="true"
+        >
+          <div className="h-56 w-56 rounded-full border-[20px] border-purple-700 blur-[80px]"></div>
+        </div>
+      </div>
+    </footer>
+  );
+}
